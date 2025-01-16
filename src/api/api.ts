32487bba9b7fad919b92cc39d2cc6a3ef0aba1b5
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2025-01-17 02:25:47 +0300

    feat: add api request

---
+++ src/api/api.ts
@@ -0,0 +1,28 @@
+import { QueryParams } from '../types/types';
+import { BASE_URL } from './constants';
+
+export const getRepositories = async (params: QueryParams) => {
+  try {
+    const config = {
+      method: 'GET',
+      headers: {},
+    };
+
+    const url = new URL(BASE_URL);
+
+    url.search = new URLSearchParams(params).toString();
+
+    const response = await fetch(url.href, config);
+
+    if (!response.ok) {
+      const error = await response.text();
+      throw new Error(error);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
